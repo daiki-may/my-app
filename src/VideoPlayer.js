0a4484137922c10827f8f59cdbf8b09ecbeb7def
daiki-may <mayumi.daiki.mb9@is.naist.jp>
2025-01-30 18:08:05 +0100

    Add VideoPlayer component

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -1,12 +1,30 @@
 import { useState } from "react";
 
-const videos = {
-  grape :"https://drive.google.com/uc?id=1gw6a2VyGLkBiEXAkFVRfqVT_GMhAwBer",
-  kiwi :"https://drive.google.com/uc?id=1mR3pKG6FEhS2Xyw5tkz0Po0a_-WpngeD",
-  melon :"https://drive.google.com/uc?id=1WtVy6Il4qa2lOFKPDt4BxpC7yKkfSBgT",
-  pine :"https://drive.google.com/uc?id=1wRhLTMJH2gzKt6Qg7coFkUsybQqUDzB2",
-  raspberry :"https://drive.google.com/uc?id=1rQ9xQqVQWRmWxjbt729QRqn_SYb4HURI",
-  strawberry :"https://drive.google.com/uc?id=1b9Fy0Q7bIA8J2wrOVibutdKoSYj4aQBI",
+const assets = {
+  grape: {
+    image: `${process.env.PUBLIC_URL}/images/grape.png`,  // ローカル画像
+    video: "https://drive.google.com/uc?id=1gw6a2VyGLkBiEXAkFVRfqVT_GMhAwBer"  // Google Drive 動画
+  },
+  kiwi: {
+    image: `${process.env.PUBLIC_URL}/images/kiwi.png`,
+    video: "https://drive.google.com/uc?id=1mR3pKG6FEhS2Xyw5tkz0Po0a_-WpngeD"
+  },
+  melon: {
+    image: `${process.env.PUBLIC_URL}/images/melon.png`,
+    video: "https://drive.google.com/uc?id=1WtVy6Il4qa2lOFKPDt4BxpC7yKkfSBgT"
+  },
+  pine: {
+    image: `${process.env.PUBLIC_URL}/images/pine.png`,  // ローカル画像
+    video: "https://drive.google.com/uc?id=1wRhLTMJH2gzKt6Qg7coFkUsybQqUDzB2"  // Google Drive 動画
+  },
+  raspberry: {
+    image: `${process.env.PUBLIC_URL}/images/raspberry.png`,
+    video: "https://drive.google.com/uc?id=1rQ9xQqVQWRmWxjbt729QRqn_SYb4HURI"
+  },
+  strawberry: {
+    image: `${process.env.PUBLIC_URL}/images/strawberry.png`,
+    video: "https://drive.google.com/uc?id=1b9Fy0Q7bIA8J2wrOVibutdKoSYj4aQBI"
+  }
 };
 
 export default function VideoPlayer() {
@@ -15,6 +33,7 @@ export default function VideoPlayer() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
       <div className="w-full max-w-3xl flex flex-col items-center">
+        {/* 動画表示エリア */}
         <div className="w-full h-64 bg-black flex items-center justify-center rounded-lg shadow-lg overflow-hidden">
           {selectedVideo ? (
             <video key={selectedVideo} controls autoPlay className="w-full h-full object-contain">
@@ -25,14 +44,15 @@ export default function VideoPlayer() {
           )}
         </div>
 
+        {/* 画像ボタン */}
         <div className="grid grid-cols-3 gap-4 mt-6">
-          {Object.keys(videos).map((fruit) => (
+          {Object.keys(assets).map((fruit) => (
             <button
               key={fruit}
               className="p-2 bg-white rounded-lg shadow hover:shadow-lg transition"
-              onClick={() => setSelectedVideo(videos[fruit])}
+              onClick={() => setSelectedVideo(assets[fruit].video)}
             >
-              <img src={`/images/${fruit}.png`} alt={fruit} className="w-24 h-24 object-contain" />
+              <img src={assets[fruit].image} alt={fruit} className="w-24 h-24 object-contain" />
               <p className="text-center mt-2 font-semibold">{fruit}</p>
             </button>
           ))}
@@ -41,3 +61,4 @@ export default function VideoPlayer() {
     </div>
   );
 }
+
